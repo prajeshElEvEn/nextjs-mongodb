79e66253d68cea0b8373768d5eafecbdf4de4d12
Prajesh Pratap Singh <prajesh.eleven118@gmail.com>
2024-02-03 13:47:34 +0530

    added shadcn ui

---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="container">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        CRUD
+      </h1>
+    </header>
+  );
+}
